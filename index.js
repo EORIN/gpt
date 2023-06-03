e55acd410dc7c99e2fa36e07d1ae493bf23146d6
Mikhail Shelestov <m.shelestov@trackad.ai>
2023-06-03 13:14:44 +0400

    Fixed __dirname constant Cleaned up code

--- index.js
+++ index.js
@@ -1,48 +1,29 @@
-import express from 'express'
-import fs from 'fs'
-// import translate from "translate";
-import bodyParser from "body-parser";
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import bodyParser from 'body-parser';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const app = express()
-// app.use(express.static(__dirname + "/"));
+const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/form.html');
+});
 
-app.get('/', (req, res) => {
+app.post('/post', (request, response) => {
 
-    res.sendFile('C:\\Users\\ur_mom\\PhpstormProjects\\gpt\\form.html')
-})
-app.post('/post', (req, res) => {
-    // let translated = async () => {
-    //     return await translate(req.body.data, 'en')
-    // }
-    // let reqText = req.body.data
-    // let translate = new Translater();
-    // translate.translateToEng(reqText)
+    fs.writeFileSync(__dirname + '/gpt_text/gpt.txt', request.body.data);
+    fs.readFile(__dirname + `/gpt_text/gpt_f.txt`, 'utf8', (err, data) => {
+        response.send(data);
+    });
+});
 
-
-    // Translater.translated().then(r => fs.writeFileSync(`./gpt.txt`, r))
-    // fs.readFile(`./gpt_f.txt`, 'utf8', (err, data) => {
-
-    // let result = async () => {
-    //
-    //     return await translate(data, 'ru')
-    // }
-    // console.log(result())
-    // res.send(result())
-    // })
-    fs.writeFileSync('C:\\Users\\ur_mom\\PhpstormProjects\\gpt\\gpt.txt', req.body.data)
-    fs.readFile(`C:\\Users\\ur_mom\\PhpstormProjects\\gpt\\gpt_f.txt`, 'utf8', (err, data) => {
-
-        res.send(data)
-
-
-    })})
-
-
-    app.listen('3333', () => {
-        console.log('i am working')
-    })
+app.listen('3333', () => {
+    console.log('i am working');
+});
