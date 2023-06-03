d96da832949edf5721bc25e99041a8bb5b736183
Mikhail Shelestov <m.shelestov@trackad.ai>
2023-06-03 13:27:33 +0400

    Added translation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 import bodyParser from 'body-parser';
+import translate from "translate";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,6 +18,23 @@ app.get('/', (request, response) => {
 });
 
 app.post('/post', (request, response) => {
+    let translated = async () => {
+        return await translate(request.body.data, 'en')
+    }
+    let requestText = request.body.data
+    let translate = new Translater();
+    translate.translateToEng(requestText)
+
+    Translater.translated().then(r => fs.writeFileSync(`./gpt.txt`, r))
+    fs.readFile(`./gpt_f.txt`, 'utf8', (err, data) => {
+
+    let result = async () => {
+
+        return await translate(data, 'ru')
+    }
+    console.log(result())
+    res.send(result())
+    })
 
     fs.writeFileSync(__dirname + '/gpt_text/gpt.txt', request.body.data);
     fs.readFile(__dirname + `/gpt_text/gpt_f.txt`, 'utf8', (err, data) => {
